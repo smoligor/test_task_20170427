3a45fccede03798c521f12cfef82e57f83d81925
smoligor <smoligor@ukr.net>
2017-05-04 14:30:14 +0300

    break lines fix

--- src/main/java/com/opinta/entity/Shipment.java
+++ src/main/java/com/opinta/entity/Shipment.java
@@ -8,9 +8,10 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.JoinColumn;
+import javax.persistence.Id;
+
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
