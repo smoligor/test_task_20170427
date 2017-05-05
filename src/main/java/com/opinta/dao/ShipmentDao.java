7460fae08704a185eff311411aa35fb41252f84f
smoligor <smoligor@ukr.net>
2017-05-05 17:32:24 +0300

    additional fixes

--- src/main/java/com/opinta/dao/ShipmentDao.java
+++ src/main/java/com/opinta/dao/ShipmentDao.java
@@ -2,6 +2,7 @@ package com.opinta.dao;
 
 import com.opinta.entity.Client;
 import com.opinta.entity.Shipment;
+
 import java.util.List;
 
 public interface ShipmentDao {
--- src/main/java/com/opinta/dao/ShipmentDaoImpl.java
+++ src/main/java/com/opinta/dao/ShipmentDaoImpl.java
@@ -8,6 +8,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
