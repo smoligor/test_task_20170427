2757a516c8aea217a61862523e6a115fa82e1775
smoligor <smoligor@ukr.net>
2017-05-05 16:40:14 +0300

    additional fixes

--- src/main/java/com/opinta/mapper/ParcelMapper.java
+++ src/main/java/com/opinta/mapper/ParcelMapper.java
@@ -3,9 +3,7 @@ package com.opinta.mapper;
 import com.opinta.dto.ParcelDto;
 import com.opinta.entity.Parcel;
 import org.mapstruct.Mapper;
-import org.springframework.stereotype.Component;
 
-@Component
 @Mapper(componentModel = "spring")
 public interface ParcelMapper extends BaseMapper<ParcelDto, Parcel> {
 }
