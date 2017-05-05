9126f21a893e9b760ac7c3c933ae23c76f13f0ee
smoligor <smoligor@ukr.net>
2017-05-05 17:08:17 +0300

    additional fixes

--- src/test/java/integration/helper/TestHelper.java
+++ src/test/java/integration/helper/TestHelper.java
@@ -3,7 +3,6 @@ package integration.helper;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-
 import com.opinta.entity.Address;
 import com.opinta.entity.Client;
 import com.opinta.entity.Counterparty;
